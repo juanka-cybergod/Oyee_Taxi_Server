859c46cddfaef2049efd9dd4086a60a5957b491c
Juan Carlos <juancarlostech891028@gmail.com>
2022-05-30 16:46:28 +0200

    - Dependencias Añadidas y Organizadas

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,26 +18,43 @@ repositories {
 
 dependencies {
 
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.projectlombok:lombok:1.18.24")
+	implementation("org.springframework.boot:spring-boot-starter-web") // Spring Web
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin") //Kotlin
+	implementation("org.jetbrains.kotlin:kotlin-reflect") //Kotlin
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8") //Kotlin
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor") //Kotlin Coroutines
+	implementation("org.projectlombok:lombok:1.18.24") //Lombok for Java Annotations
 
+	//USING
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb") //MongoDBDB
+	implementation("com.twilio.sdk:twilio:8.29.1") //Twillio
+	implementation("org.springframework.boot:spring-boot-starter-mail") //Spring Java Mail Sender
+	implementation("org.springframework.boot:spring-boot-starter-security") //Spring Security
+
+	//NOT USING
+	//runtimeOnly("mysql:mysql-connector-java") No Used //MySQL
+	implementation("org.springframework.boot:spring-boot-starter-websocket") //Spring Web Socket
+	implementation("io.socket:socket.io-server:4.0.1") //Socket.IO
+	implementation("io.springfox:springfox-boot-starter:3.0.0") //Swagger
+	implementation("io.springfox:springfox-swagger-ui:3.0.0") //Swagger UI
+	implementation("org.springframework.boot:spring-boot-starter-webflux") //Spring Reactive Web WebClients for external Http Request
+
+	//Retrofit
+	implementation("com.squareup.retrofit2:retrofit:2.9.0")
+	implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+	implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
+	implementation("com.squareup.okhttp3:okhttp:4.9.3")
+
+	//Gson
+	implementation("com.google.code.gson:gson:2.9.0")
+
+	//Test
 	implementation("junit:junit:4.13.2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-
-	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-	//runtimeOnly("mysql:mysql-connector-java") No Used
-
-	implementation("com.twilio.sdk:twilio:8.29.1")
-	implementation("org.springframework.boot:spring-boot-starter-mail")
-	implementation("org.springframework.boot:spring-boot-starter-security")
 	testImplementation("org.springframework.security:spring-security-test")
-	implementation("org.springframework.boot:spring-boot-starter-mail")
+	testImplementation("io.projectreactor:reactor-test")
 
-	implementation("io.socket:socket.io-server:4.0.1")
-	implementation("org.springframework.boot:spring-boot-starter-websocket")
+
 
 }
 
